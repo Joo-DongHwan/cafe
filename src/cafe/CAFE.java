4420ba40bbc7763d15b7e231f548042fac7b778f
Joo-DongHwan <iteunjin0919@nate.com>
2021-06-07 11:37:33 +0900

    카페 메뉴 문자열 출력 기능 추가

--- src/cafe/CAFE.java
+++ src/cafe/CAFE.java
@@ -1,11 +1,18 @@
 package cafe;
 
+import java.util.Arrays;
+
 public class CAFE {
 
 	public static void main(String[] args) {
 		TeaType tea = TeaType.보성녹차;
 		System.out.println("Cafe에 어서오세요");
-		System.out.println(tea);
+		TeaType teaTypes[] = TeaType.values();
+		String menu = String.join("/",
+				TeaType.감잎차.toString(),
+				TeaType.보성녹차.toString(),
+				TeaType.율무차.toString());
+		System.out.println(menu);
 
 	}
 
